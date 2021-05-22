8b18b9c487ef5c91f1495cb34d02416a0cdb7d9a
Edgar HiSo <edghiso@gmail.com>
2021-05-22 09:20:44 +0200

    lab done

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+const { request, response } = require('express');
 const express = require('express');
 const app = express();
+app.use(express.static('public'))
+const dataEpisodes = require('./data/got.json')
 
 
 /**
  * View setup
  */
-app.set('views', __dirname + 'views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
+
+app.get("/home", (request, response) => {
+    response.render("home" , {
+        data: dataEpisodes
+    })
+})
+
 
 
 const port = 3000;
